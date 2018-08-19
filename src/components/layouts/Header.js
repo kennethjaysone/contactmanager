d92b0745a4c37bf4e3f0a3be1d58dcf55692932d
Kenneth Jaysone Francis <kennethjaysone@gmail.com>
2018-08-19 13:33:23 +0800

    added gh-pages and linked to home of gh-pages url for app

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -8,13 +8,13 @@ const Header = props => {
   return (
     <nav className="navbar navbar-inverse navbar-expand-sm navbar-darl bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <a href="/contactmanager" className="navbar-brand">
           {branding}
         </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <Link to="/contactmanager" className="nav-link">
                 <i className="fas fa-home" /> Home
               </Link>
             </li>
